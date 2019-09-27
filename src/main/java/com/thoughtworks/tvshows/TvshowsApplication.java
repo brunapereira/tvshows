c531d73493368bd16045deaddda95c8167742f92
Bruna Pereira <bruna_pereira@external.mckinsey.com>
2019-09-27 12:09:07 -0300

    Add checkstyle

--- src/main/java/com/thoughtworks/tvshows/TvshowsApplication.java
+++ src/main/java/com/thoughtworks/tvshows/TvshowsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TvshowsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TvshowsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TvshowsApplication.class, args);
+    }
 }
